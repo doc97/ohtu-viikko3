7fa070c0146f939cb17cbd866a3256e31009f678
doc97 <daniel.riissanen@gmail.com>
2018-11-14 12:24:42 +0200

    Prettify submission output

--- Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,37 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static void printStudentData(String studentNr, Submission[] submissions) {
+        int nExercises = 0;
+        int nHours = 0;
+        System.out.println("opiskelijanumero: " + studentNr + "\n");
+        for (Submission sub : submissions) {
+            nExercises += sub.getExerciseCount();
+            nHours += sub.getHours();
+            System.out.println("  " + sub);
+        }
+        System.out.println("\nYhteensä: " + nExercises + " tehtävää, " + nHours + " tuntia");
+    }
+
+    public static void main(String[] args) throws IOException {
+        // ÄLÄ laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        printStudentData(studentNr, subs);
     }
 }
--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,19 +1,33 @@
 package ohtu;
 
-public class Submission {
-    private int week;
+import java.util.Arrays;
 
-    public void setWeek(int week) {
-        this.week = week;
+public class Submission {
+
+    private int week;
+    private int hours;
+    private int[] exercises;
+    private String course;
+
+    private Submission() {
+        exercises = new int[0];
     }
 
-    public int getWeek() {
-        return week;
+    public int getHours() {
+        return hours;
+    }
+
+    public int getExerciseCount() {
+        return exercises.length;
     }
 
     @Override
     public String toString() {
-        return ""+week;
+        StringBuilder builder = new StringBuilder();
+        builder.append(course).append(" (viikko ").append(week).append("): ")
+                .append("Tehtäviä tehty ").append(exercises.length)
+                .append(", aikaa käytetty ").append(hours).append(" tuntia")
+                .append(", tehtävät: ").append(Arrays.toString(exercises));
+        return builder.toString();
     }
-    
 }
