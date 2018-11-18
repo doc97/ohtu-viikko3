7be4bd712e762368316d14235d706fca22003134
doc97 <daniel.riissanen@gmail.com>
2018-11-18 22:30:19 +0200

    Add reset counter cucumber test

--- HelloCucumber/src/test/java/ohtu/Stepdefs.java
+++ HelloCucumber/src/test/java/ohtu/Stepdefs.java
@@ -22,6 +22,11 @@ public class Stepdefs {
     public void it_is_incremented_by(int val) throws Throwable {
         counter.increment(val);
     }
+    
+    @When("^it is reset$")
+    public void it_is_reset() throws Throwable {
+        counter.reset();
+    }
 
     @Then("^the value should be (\\d+)$")
     public void the_value_should_be(int val) throws Throwable {
